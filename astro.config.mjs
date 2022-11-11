eb41192fc4a3f639e51f6d9515f4e43bf5a69197
Andrés Castellanos <me@andrescv.dev>
2022-11-10 21:39:10 -0600

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://andrescv.dev',
+  integrations: [sitemap(), tailwind()],
 });
